87a76bba04599d860030b1eba8592950250ec343
AmberLs <93353341+AmJoy01@users.noreply.github.com>
2022-06-12 12:16:23 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-📝 `NOTE` Use this template to initialize the contents of a README.md file for your application. As you work on your assignment over the course of the week, update the required or stretch features lists to indicate which features you have completed by changing `[ ]` to `[x]`. (🚫 Remove this paragraph before submitting your assignment.)
-
 ## Week 1 Assignment: Flixster
 
 Submitted by: **Amber Lewis**
 
-Estimated time spent: **#** hours spent in total
+Estimated time spent: **21** hours spent in total
 
 Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
@@ -19,7 +17,7 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
   - Title - the element should have a class of `movie-title`
   - Image - the `img` element should have a class of `movie-poster`
   - Votes - the element should have a class of `movie-votes`
-- [ ] User can load more current movies by clicking a button at the bottom of the list
+- [x] User can load more current movies by clicking a button at the bottom of the list
   - The button should have an id of `load-more-movies-btn`.
   - When clicked, the page should not refresh.
   - New movies should simply be added to the bottom
@@ -29,8 +27,8 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
   - When a user hits 'Enter', it should send a search request to the movies API
   - The results from the search should be displayed on the page
   - There should be a close icon with an id of `close-search-btn` that exits the search, clears results, and shows the current movies displayed previously
-- [ ] Website accounts for basic HTML/CSS accessibility features
-- [ ] Website should be responsive
+- [x] Website accounts for basic HTML/CSS accessibility features
+- [x] Website should be responsive
 
 #### STRETCH FEATURES
 
@@ -44,26 +42,33 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
 `TODO://` Add the embedded URL code to your animated app walkthrough below, `ADD_EMBEDDED_CODE_HERE`. Make sure the video or gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after adding walkthrough video)
 
-`ADD_EMBEDDED_CODE_HERE`
+<a href="https://s3.amazonaws.com/img0.recordit.co/3bWzuA8Jrk.mp4?AWSAccessKeyId=AKIAUQ5RURZ7ND2T2B6I&Expires=1655053007&Signature=IbHICk3XKNL6t40ApYPfxyqKI%2FA%3D">Gif me</a>
 
 ### Reflection
 
 - Did the topics discussed in your labs prepare you to complete the assignment? Be specific, which features in your weekly assignment did you feel unprepared to complete?
 
-Add your response here
+Yes. The 3rd Lab especially prepared me on how to use API and Javascript.
 
 - If you had more time, what would you have done differently? Would you have added additional features? Changed the way your project responded to a particular event, etc.
 
-Add your response here
+If I had more time, I want to clean up my javascript code and brush up on CSS and Bootstrap to make the site pretty.
 
 - Reflect on your project demo, what went well? Were there things that maybe didn't go as planned? Did you notice something that your peer did that you would like to try next time?
 
-Add your response here
+The API went well. I didn't have a problem with gridding my movies. My peers put in a go back up button which I would love to utilize next time.
 
 ### Open-source libraries used
 
-- Add any links to open-source libraries used in your project.
+- <a href="https://developer.mozilla.org/en-US/">MDN</a>
+- <a href="https://www.w3schools.com/css/css_grid.asp">W3Schools CSS Grid Layout</a>
 
 ### Shout out
 
 Give a shout out to somebody from your cohort that especially helped you during your project. This can be a fellow peer, instructor, TA, mentor, etc.
+
+Kevin
+Rebecca Li
+Maurice August
+James Bosch
+Anitya (Pep talk)
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const MOVIE_API_KEY = "6a16fb7543675c64129939e587e41a8e";
 
 let form = document.querySelector("#playingForm");
-
 let searchBar = document.querySelector("#searchBar");
-let movieCard = document.querySelector(".movie-card");
+let movieCard = document.querySelector("#movies-grid");
 let image = document.querySelector("#movie-poster");
-let userInput = document.querySelector("textBar");
 let searchForm = document.querySelector("#searchForm");
-let loadBtn = document.querySelector("#loadFrom");
+let loadBtn = document.querySelector("#loadForm");
+let clearBtn = document.querySelector('#clearForm');
 
+let pages = 1;
 // display now playing movies
 form.addEventListener("click", getNowPlayingMovies);
 
@@ -17,21 +17,12 @@ async function getNowPlayingMovies(evt) {
     let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${MOVIE_API_KEY}&language=en-US&page=1`);
     let responseData = await response.json();
     console.log(responseData);
-    displayNowPlayingMovies(responseData);
+    displayMovies(responseData);
     document.querySelector("#nowPlayingBtn").classList.add("hidden");
+    document.querySelector("#clearBtn").classList.add("hidden");
 
     //remove hidden load button
-    document.querySelector("#loadBtn").classList.remove("hidden");
-
-}
-
-function displayNowPlayingMovies(movieData) {
-    movieCard.innerHTML = "";
-    for (let i = 0; i < movieData.results.length; i++) {
-        movieCard.innerHTML += `<div id="movie-poster"><img src="http://image.tmdb.org/t/p/w500${movieData.results[i].poster_path}" alt="" /></div>
-    <div class="movie-title">${movieData.results[i].title}</div>
-    <div class="movie-votes">${movieData.results[i].vote_average}</div>`;
-    }
+    document.querySelector("#loadBtn").classList.remove("hidden")
 }
 
 // search movies
@@ -47,20 +38,64 @@ async function getSearchMovies(evt) {
     // console.log("response", response); OK
     let responseData = await response.json();
     console.log("responseData", responseData);
-    displaySearchMovies(responseData);
+    movieCard.innerHTML = "";
+    displayMovies(responseData);
     document.querySelector("#nowPlayingBtn").classList.remove("hidden");
-    // for(int i = 0; i < responseData.results.length; i++){
-
-    // }
+    document.querySelector("#clearBtn").classList.remove("hidden");
+    document.querySelector("#loadBtn").classList.add("hidden");
 }
 
-function displaySearchMovies(movieData) {
-    movieCard.innerHTML = "";
+function displayMovies(movieData) {
+    // movieCard.innerHTML = "";
+    let movieCard1;
     for (let i = 0; i < movieData.results.length; i++) {
-        movieCard.innerHTML += `<div id="movie-poster"><img src="http://image.tmdb.org/t/p/w500${movieData.results[i].poster_path}" alt="" /></div>
-    <div class="movie-title">${movieData.results[i].title}</div>
-    <div class="movie-votes">${movieData.results[i].vote_average}</div>`;
-        console.log(i);
+        movieCard1 = document.createElement("div");
+        movieCard1.setAttribute("class", "movie-card")
+        movieCard1.innerHTML += `<div id="movie-poster"><img src="http://image.tmdb.org/t/p/w500${movieData.results[i].poster_path}" alt="" /></div>
+        <div class="movie-title">${movieData.results[i].title}</div>
+        <div class="movie-votes">${movieData.results[i].vote_average}</div>`;
+        document.getElementById("movies-grid").appendChild(movieCard1);
+        // console.log(i);
     }
 
+}
+
+loadBtn.addEventListener("click", getLoadNowPlayingMovies);
+async function getLoadNowPlayingMovies(evt) {
+    pages += 1;
+    evt.preventDefault();
+    console.log(evt);
+    let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${MOVIE_API_KEY}&language=en-US&page=${pages}`);
+    let responseData = await response.json();
+    console.log(responseData);
+    displayMovies(responseData);
+    if (pages == 5) {
+        document.querySelector("#loadBtn").classList.add("hidden");
+    }
+}
+
+// function displayLoadMovies(movieData) {
+//     for (let i = 0; i < movieData.results.length; i++) {
+//         movieCard.innerHTML += `<div id="movie-poster"><img src="http://image.tmdb.org/t/p/w500${movieData.results[i].poster_path}" alt="" /></div>
+//     <div class="movie-title">${movieData.results[i].title}</div>
+//     <div class="movie-votes">${movieData.results[i].vote_average}</div>`;
+//         console.log(i);
+//     }
+
+// }
+
+clearBtn.addEventListener("click", getBackNowPlayingMovies);
+
+async function getBackNowPlayingMovies(evt) {
+    evt.preventDefault();
+    let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${MOVIE_API_KEY}&language=en-US&page=1`);
+    let responseData = await response.json();
+    console.log(responseData);
+    movieCard.innerHTML = "";
+    displayMovies(responseData);
+    document.querySelector("#nowPlayingBtn").classList.add("hidden");
+    document.querySelector("#clearBtn").classList.add("hidden");
+
+    //remove hidden load button
+    document.querySelector("#loadBtn").classList.remove("hidden")
 }
